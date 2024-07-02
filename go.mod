d31d76ebf826c18179aca33cd3bd62aeae4c6f05
Alexandr <1402084@gmail.com>
2024-07-02 21:42:01 +0300

    create context.go

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module clean/architector
 
 go 1.22.1
 
-require github.com/go-chi/chi/v5 v5.0.14
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.14
+)
--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-
 	"clean/architector/internal/adapters/productrepo/postgresql"
+	"clean/architector/internal/app"
 	"clean/architector/internal/core/product"
 )
 
@@ -15,14 +11,11 @@ type IproductUseCase interface {
 }
 
 func main() {
+
+var c *app.Context = app.NewContext()
+c.StartWebServer()
+
 	var productId int = 2
 	var iProductUseCase IproductUseCase = product.NewViewProductUseCase(postgresql.NewProductRepo(productId))
 	iProductUseCase.Run(productId)
-
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
-	http.ListenAndServe("localhost:3000", r)
 }
