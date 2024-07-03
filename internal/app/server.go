c5b59d709a911d2497baea32e4017583e1c8c2f1
Proskuryakov_Alexandr <1402084@gmail.com>
2024-07-03 16:40:34 +0300

    add handler function addMsgInKafka

--- internal/app/server.go
+++ internal/app/server.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -18,9 +20,28 @@ func NewServer(cfg *Config) *Server {
 func (c *Server) StartServer() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/kafka/topic/:topic_name", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
+	r.Post("/kafka/topic/{topic_name}", addMsgInKafka)
 
 	http.ListenAndServe(c.Host, r)
+}
+
+func addMsgInKafka(w http.ResponseWriter, r *http.Request) {
+	// topicName := chi.URLParam(r, "topic_name")
+
+	var data Data
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&data)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(data)
+
+}
+
+type Data struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
