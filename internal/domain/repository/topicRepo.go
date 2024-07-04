8c139de7769fdd5b0e153faa1285256042a098d1
Alexandr <1402084@gmail.com>
2024-07-04 20:44:08 +0300

    create module topic

--- internal/domain/repository/topicRepo.go
+++ internal/domain/repository/topicRepo.go
@@ -2,9 +2,8 @@ package repository
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 type TopicRepo struct {
@@ -12,47 +11,40 @@ type TopicRepo struct {
 	MsgTopic  string
 }
 
-func NewMsgTopic(topicId string, msgTopic string) *TopicRepo {
+func InitTopicRepo(topicId string, msgTopic string) *TopicRepo {
 	return &TopicRepo{TopicName: topicId, MsgTopic: msgTopic}
 }
 
-func (t *TopicRepo) AddMsgToTopic() {
+func (t *TopicRepo) NewMsgToTopic() {
 
-	fmt.Println(t)
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "0.0.0.0:29092"})
 
-	p, err := kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers": "0.0.0.0:29092",
-		"client.id":         "localhost",
-		"acks":              "all"})
+		if err != nil {
+			panic(err)
+		}
 
-	if err != nil {
-		fmt.Printf("Failed to create producer: %s", err)
-		os.Exit(1)
-	}
+		defer p.Close()
 
-	// Go-routine to handle message delivery reports and
-	// possibly other event types (errors, stats, etc)
+	// Delivery report handler for produced messages
 	go func() {
 		for e := range p.Events() {
 			switch ev := e.(type) {
 			case *kafka.Message:
 				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
 				} else {
-					fmt.Printf("Produced event to topic %s: key = %-10s value = %s\n",
-						*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
 				}
 			}
 		}
 	}()
 
 	p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &t.MsgTopic, Partition: kafka.PartitionAny},
-		Key:            []byte(t.TopicName),
+		TopicPartition: kafka.TopicPartition{Topic: &t.TopicName, Partition: kafka.PartitionAny},
 		Value:          []byte(t.MsgTopic),
 	}, nil)
 
+	// Wait for message deliveries before shutting down
 	p.Flush(3 * 1000)
-	p.Close()
 
 }
