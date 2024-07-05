c22f6c15a5c8b6f347295a230bf9e362980a361d
Alexandr <1402084@gmail.com>
2024-07-05 10:34:47 +0300

    add topic handler

--- internal/app/server.go
+++ internal/app/server.go
@@ -1,14 +1,10 @@
 package app
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"clean/architector/internal/domain/adapter"
-	"clean/architector/internal/domain/entitie"
-	"clean/architector/internal/domain/repository"
-	"clean/architector/internal/domain/usecase"
+	"clean/architector/internal/ui/web"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -26,30 +22,6 @@ func (c *Server) StartServer() {
 	fmt.Println("start server")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/kafka/topic/{topic_name}", sendMsgToTopicHandler)
-
+	r.Post("/kafka/topic/{topic_name}", web.AddMsgToTopicHandler)
 	http.ListenAndServe(c.Host, r)
-}
-
-func sendMsgToTopicHandler(w http.ResponseWriter, r *http.Request) {
-	var topicName string = chi.URLParam(r, "topic_name")
-
-	if topicName == "" {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	var newMsgTopic entitie.MsgTopic
-	decoder := json.NewDecoder(r.Body)
-
-	err := decoder.Decode(&newMsgTopic)
-	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	var iTopicrepo adapter.ItopicRepo = repository.InitTopicRepo(topicName, newMsgTopic.Msg)
-	var topicUseCase *usecase.TopicUseCase = usecase.InitTopicUseCase(iTopicrepo)
-	topicUseCase.SendMsgToTopic()
-
 }
